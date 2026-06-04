f01e5422ecda3cd7353542dec03dc329e75e897a
Akobir <akobirabduvahobov@gmail.com>
2026-06-04 16:01:19 +0500

    dtos are created for lesson

--- LMSPro/src/LMSPro.Api/Dtos/LessonCreateDto.cs
+++ LMSPro/src/LMSPro.Api/Dtos/LessonCreateDto.cs
@@ -1,6 +1,17 @@
-﻿namespace LMSPro.Api.Dtos
+﻿using LMSPro.Api.Entities;
+
+namespace LMSPro.Api.Dtos
 {
     public class LessonCreateDto
     {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int Order { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public long CourseId { get; set; }
     }
 }
