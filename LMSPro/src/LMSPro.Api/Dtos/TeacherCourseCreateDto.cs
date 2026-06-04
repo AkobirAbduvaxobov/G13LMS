ee6d644cc21c9eeae762364232104daa6de9642f
YoldoshevM <yoldoshevm396@gmail.com>
2026-06-04 16:50:20 +0500

    TeacherCourse Dtos created

---
+++ LMSPro/src/LMSPro.Api/Dtos/TeacherCourseCreateDto.cs
@@ -0,0 +1,9 @@
+﻿using LMSPro.Api.Entities;
+
+namespace LMSPro.Api.Dtos;
+
+public class TeacherCourseCreateDto
+{
+    public long TeacherId { get; set; }
+    public long CourseId { get; set; }
+}
---
+++ LMSPro/src/LMSPro.Api/Dtos/TeacherCourseUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LMSPro.Api.Dtos;
+
+public class TeacherCourseUpdateDto
+{
+    public long TeacherCourseId { get; set; }
+    public long TeacherId { get; set; }
+    public long CourseId { get; set; }
+}
