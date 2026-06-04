47e3e102611a6fc65e6478ff5711d5619e188389
rasullbe <rasulbek112208@gmail.com>
2026-06-04 16:35:27 +0500

    added missed code

--- LMSPro/src/LMSPro.Api/Dtos/ExamUpdateDto.cs
+++ LMSPro/src/LMSPro.Api/Dtos/ExamUpdateDto.cs
@@ -3,6 +3,6 @@ namespace LMSPro.Api.Dtos;
 public class ExamUpdateDto
 {
     public string Title { get; set; }
-
+    public long LessonId { get; set; }
     public int PassingScorePercentage { get; set; }
 }
