d91f5f80d9eca8d60b62b349d075bd4b347a1ae4
YoldoshevM <yoldoshevm396@gmail.com>
2026-06-05 22:31:45 +0500

    ExamMapper created

---
+++ LMSPro/src/LMSPro.Api/Mappings/ExamMapper.cs
@@ -0,0 +1,34 @@
+﻿using LMSPro.Api.Dtos;
+using LMSPro.Api.Entities;
+
+namespace LMSPro.Api.Mappings;
+
+public static class ExamMapper
+{
+    public static Exam ToEntity(this ExamCreateDto dto)
+    {
+        return new Exam
+        {
+            Title = dto.Title,
+            PassingScorePercentage = dto.PassingScorePercentage,
+            LessonId = dto.LessonId
+        };
+    }
+
+    public static ExamGetDto ToGetDto(this Exam exam)
+    {
+        return new ExamGetDto
+        {
+            ExamId = exam.ExamId,
+            Title = exam.Title,
+            PassingScorePercentage = exam.PassingScorePercentage,
+            LessonId = exam.LessonId
+        };
+    }
+
+    public static void UpdateEntity(this ExamUpdateDto dto, Exam exam)
+    {
+        exam.Title = dto.Title;
+        exam.PassingScorePercentage = dto.PassingScorePercentage;
+    }
+}
