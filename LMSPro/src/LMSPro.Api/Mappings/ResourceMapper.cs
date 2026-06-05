5039d76f302c3db2708759219d919301e8e05ca5
YoldoshevM <yoldoshevm396@gmail.com>
2026-06-05 22:17:39 +0500

    ResourceMapper created

---
+++ LMSPro/src/LMSPro.Api/Mappings/ResourceMapper.cs
@@ -0,0 +1,37 @@
+﻿using LMSPro.Api.Dtos;
+using LMSPro.Api.Entities;
+
+namespace LMSPro.Api.Mappings;
+
+public static class ResourceMapper
+{
+    public static Resource ToEntity(this ResourceCreateDto dto)
+    {
+        return new Resource
+        {
+            Name = dto.Name,
+            Url = dto.Url,
+            Type = dto.Type,
+            LessonId = dto.LessonId
+        };
+    }
+
+    public static ResourceGetDto ToGetDto(this Resource resource)
+    {
+        return new ResourceGetDto
+        {
+            ResourceId = resource.ResourceId,
+            Name = resource.Name,
+            Url = resource.Url,
+            Type = resource.Type,
+            LessonId = resource.LessonId
+        };
+    }
+
+    public static void UpdateEntity(this ResourceUpdateDto dto, Resource resource)
+    {
+        resource.Name = dto.Name;
+        resource.Url = dto.Url;
+        resource.Type = dto.Type;
+    }
+}
