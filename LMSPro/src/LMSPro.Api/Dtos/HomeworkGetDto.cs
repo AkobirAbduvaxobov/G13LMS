da12181fe6a82ebe0e9def49c364cc711078a165
e1shod <hackergemer9@gmail.com>
2026-06-06 14:49:51 +0500

    WI134 Create mappings for HOMEWORK

--- LMSPro/src/LMSPro.Api/Dtos/HomeworkGetDto.cs
+++ LMSPro/src/LMSPro.Api/Dtos/HomeworkGetDto.cs
@@ -2,5 +2,14 @@
 {
     public class HomeworkGetDto
     {
+        public long HomeworkId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public long LessonId { get; set; }
+
+        public string? LessonTitle { get; set; }
     }
 }
---
+++ LMSPro/src/LMSPro.Api/Mappings/HomeworkMapper.cs
@@ -0,0 +1,25 @@
+﻿using LMSPro.Api.Dtos;
+using LMSPro.Api.Entities;
+
+namespace LMSPro.Api.Mappings;
+
+public static class HomeworkMapper
+{
+    public static HomeworkGetDto ToGetDto(this Homework homework)
+    {
+        var res = new HomeworkGetDto
+        {
+            HomeworkId = homework.HomeworkId,
+            Title = homework.Title,
+            Description = homework.Description,
+            LessonId = homework.LessonId
+        };
+
+        if (homework.Lesson != null)
+        {
+            res.LessonTitle = homework.Lesson.Title;
+        }
+
+        return res;
+    }
+}
