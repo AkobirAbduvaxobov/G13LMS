ad3f28b4e5ccbdb2f2409ad628ba02f541aaa00e
Bekzod <bekzodjonxasanov7@gmail.com>
2026-06-09 16:44:07 +0500

    DTOs Created for TeacherCourse entity

---
+++ LMSPro/src/LMSPro.Api/Dtos/TeacherCourseCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace LMSPro.Api.Dtos;
+
+public class TeacherCourseCreateDto
+{
+    public long TeacherId { get; set; }
+    public long CourseId { get; set; }
+}
---
+++ LMSPro/src/LMSPro.Api/Dtos/TeacherCourseGetDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LMSPro.Api.Dtos;
+
+public class TeacherCourseGetDto
+{
+    public long TeacherCourseId { get; set; }
+    public long TeacherId { get; set; }
+    public long CourseId { get; set; }
+}
