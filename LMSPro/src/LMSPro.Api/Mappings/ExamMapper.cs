eb73d9c15040b3352391bb4b2689601e18f54731
Bekzod <bekzodjonxasanov7@gmail.com>
2026-06-09 14:06:05 +0500

    ExamMapper added

---
+++ LMSPro/src/LMSPro.Api/Mappings/ExamMapper.cs
@@ -0,0 +1,46 @@
+﻿using LMSPro.Api.Dtos;
+using LMSPro.Api.Entities;
+
+namespace LMSPro.Api.Mappings;
+
+public static class ExamMapper
+{
+    public static ExamGetDto ToGetDto(this Exam exam)
+    {
+        var res = new ExamGetDto
+        {
+            ExamId = exam.ExamId,
+            Title = exam.Title,
+            PassingScorePercentage = exam.PassingScorePercentage,
+            LessonId = exam.LessonId
+        };
+
+        if (exam.Lesson != null)
+        {
+            res.Lesson = new LessonGetDto
+            {
+                LessonId = exam.Lesson.LessonId,
+                Title = exam.Lesson.Title,
+                Content = exam.Lesson.Content
+            };
+        }
+
+        return res;
+    }
+
+    public static Exam ToEntity(this ExamCreateDto examCreateDto)
+    {
+        return new Exam
+        {
+            Title = examCreateDto.Title,
+            PassingScorePercentage = examCreateDto.PassingScorePercentage,
+            LessonId = examCreateDto.LessonId
+        };
+    }
+
+    public static void ToUpdateEntity(this ExamUpdateDto examUpdateDto, Exam exam)
+    {
+        exam.Title = examUpdateDto.Title;
+        exam.PassingScorePercentage = examUpdateDto.PassingScorePercentage;
+    }
+}
