3315bd24a9f46cb15fa7c05389c19c76b9f9c3d4
bunyod <bunyodmusayev17@gmail.com>
2026-06-11 16:32:38 +0500

    Update Program.cs

--- LMSPro/src/LMSPro.Api/Program.cs
+++ LMSPro/src/LMSPro.Api/Program.cs
@@ -26,7 +26,6 @@ public class Program
 
         //builder.Services.AddValidatorsFromAssemblyContaining<CourseCreateDto>();
         builder.Services.AddValidatorsFromAssembly(typeof(CourseCreateDto).Assembly);
-        
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
