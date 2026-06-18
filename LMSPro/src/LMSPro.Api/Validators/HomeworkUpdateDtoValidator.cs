487ab974b79a10641e0a114dd71ab3e70182a8a8
e1shod <hackergemer9@gmail.com>
2026-06-18 23:15:49 +0500

    added

---
+++ LMSPro/src/LMSPro.Api/Validators/HomeworkUpdateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using LMSPro.Api.Dtos;
+
+namespace LMSPro.Api.Validators;
+
+public class HomeworkUpdateDtoValidator : AbstractValidator<HomeworkUpdateDto>
+{
+    public HomeworkUpdateDtoValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(200).WithMessage("Title must not exceed 200 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.");
+
+        RuleFor(x => x.LessonId)
+            .GreaterThan(0).WithMessage("Invalid LessonId.");
+    }
+}
