738c1200ae43b52f7fdabc76f683abff9d93edca
Bekzod <bekzodjonxasanov7@gmail.com>
2026-06-20 17:58:44 +0500

    TimeControl added

---
+++ LMSPro/src/LMSPro.Api/Middlewares/TimeControl.cs
@@ -0,0 +1,27 @@
+﻿namespace LMSPro.Api.Middlewares;
+
+public class TimeControl
+{
+    private readonly RequestDelegate _next;
+
+    public TimeControl(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+
+        var start = DateTime.Now;
+
+        if (start.Minute % 2 == 0)
+        {
+            await _next(context);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("omadsiz");
+        }
+    }
+}
