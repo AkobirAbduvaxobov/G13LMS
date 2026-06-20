258bcd7ddfc72f18db54480a32401809e5557303
bunyodmusayev17-beep <bunyodmusayev17@gmail.com>
2026-06-20 17:59:13 +0500

    EvenSecondMiddleware

---
+++ LMSPro/src/LMSPro.Api/Middlewares/EvenSecondMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace LMSPro.Api.Middlewares;
+
+public class EvenSecondMiddleware
+{
+    private readonly RequestDelegate Next;
+
+    public EvenSecondMiddleware(RequestDelegate next)
+    {
+        Next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var second = DateTime.Now.Second;
+
+        if (second % 2 == 0)
+        {
+            await Next(context);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("Request rejected. Current second is odd.");
+        }
+    }
+}
+
+public static class EvenSecondMiddlewareExtensions
+{
+    public static IApplicationBuilder UseEvenSecondMiddleware(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<EvenSecondMiddleware>();
+    }
+}
