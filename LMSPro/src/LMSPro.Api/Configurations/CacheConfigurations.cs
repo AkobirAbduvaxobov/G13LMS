dfdd7ff158ae5b1d558072dac1c6c55d1a764bc5
Miryoqub006 <158826997+Miryoqub006@users.noreply.github.com>
2026-06-23 17:00:36 +0500

    Caching added to Question Sevice

--- LMSPro/src/LMSPro.Api/Configurations/CacheConfigurations.cs
+++ LMSPro/src/LMSPro.Api/Configurations/CacheConfigurations.cs
@@ -1,5 +1,4 @@
 ﻿using LMSPro.Api.Configurations.Settings;
-using LMSPro.Api.Entities;
 
 namespace LMSPro.Api.Configurations
 {
@@ -9,20 +8,37 @@ namespace LMSPro.Api.Configurations
         {
             builder.Services.AddMemoryCache();
 
-            var absoluteExpirationMinutes =
+            //var cacheSettings = builder.Configuration
+            //                           .GetSection("CacheSettings")
+            //                           .Get<CacheSettings>();
+
+            var courseAbsoluteExpirationMinutes =
                 builder.Configuration["CacheSettings:Courses:AbsoluteExpirationMinutes"];
 
-            var slidingExpirationMinutes =
+            var courseSlidingExpirationMinutes =
                 builder.Configuration["CacheSettings:Courses:SlidingExpirationMinutes"];
+
+            var questionAbsoluteExpirationMinutes =
+                builder.Configuration["CacheSettings:Questions:AbsoluteExpirationMinutes"];
+
+            var questionSlidingExpirationMinutes =
+                builder.Configuration["CacheSettings:Questions:SlidingExpirationMinutes"];
 
             var cacheSettings = new CacheSettings
             {
                 Courses = new CourseCacheSettings
                 {
-                    AbsoluteExpirationMinutes = int.Parse(absoluteExpirationMinutes),
-                    SlidingExpirationMinutes = int.Parse(slidingExpirationMinutes)
+                    AbsoluteExpirationMinutes = int.Parse(courseAbsoluteExpirationMinutes),
+                    SlidingExpirationMinutes = int.Parse(courseSlidingExpirationMinutes)
+                }
+                Questions = new QuestionCacheSettings
+                {
+                    AbsoluteExpirationMinutes = int.Parse(questionAbsoluteExpirationMinutes),
+                    SlidingExpirationMinutes = int.Parse(questionSlidingExpirationMinutes)
                 }
             };
+
+
 
             builder.Services.AddSingleton(cacheSettings);
         }
