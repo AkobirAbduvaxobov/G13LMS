8fdc7c2bd48f6267f4d18a145be2b9e3557a611a
Akobir <akobirabduvahobov@gmail.com>
2026-06-30 16:16:53 +0500

    done added redis cache for students

---
+++ LMSPro/src/LMSPro.Api/Services/RedisCacheService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace LMSPro.Api.Services;
+
+public class RedisCacheService
+{
+    private readonly IDistributedCache Cache;
+
+    public RedisCacheService(IDistributedCache cache)
+    {
+        Cache = cache;
+    }
+
+    public async Task SetAsync(string key, string value, DistributedCacheEntryOptions distributedCacheEntryOptions = null)
+    {
+        await Cache.SetStringAsync(key, value, distributedCacheEntryOptions);
+    }
+
+    public async Task<string?> GetAsync(string key)
+    {
+        return await Cache.GetStringAsync(key);
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        await Cache.RemoveAsync(key);
+    }
+}
